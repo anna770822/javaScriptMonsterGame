1a8575d97c641379f810f1387ea0b2326fd01796
anna770822 <anna770822@hotmail.com>
2018-01-31 18:23:29 +0800

    updateHTML function , when get hurt hp context and hp bar will interface

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
+//    //
+
 class BaseCharacter {
-
-
 
   constructor(name,hp,ap){
     this.name = name;
@@ -26,6 +26,12 @@ class BaseCharacter {
     this.alive=false;
   }
 
+  updateHTML(hpElement,hurtElement){
+    hpElement.textContent = this.hp;
+    hurtElement.style.width = (100-this.hp/this.maxHp*100)+"%";
+
+  }
+
 
 
 }
@@ -33,12 +39,25 @@ class BaseCharacter {
 class Hero extends BaseCharacter{
   constructor(name,hp,ap){
     super(name,hp,ap);
+    this.element = document.getElementById("hero-image-block");
+    this.hpElement = document.getElementById("hero-hp");
+    this.maxHpElement = document.getElementById("hero-max-hp");
+    this.hurtElement = document.getElementById("hero-hp-hurt");
+
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHp;
+
     console.log("召喚英雄"+this.name+"!");
   }
   attack(character){
     var damage = Math.random()*(this.ap/2)+(this.ap/2);
     super.attack(character,Math.floor(damage));
   }
+  getHurt(damage){
+    super.getHurt(damage);
+    this.updateHTML(this.hpElement,this.hurtElement);
+
+    }
 
 }
 //    //
@@ -46,11 +65,25 @@ class Monster extends BaseCharacter{
 
   constructor(name,hp,ap){
     super(name,hp,ap);
+    this.element = document.getElementById("monster-image-block");
+    this.hpElement = document.getElementById("monster-hp");
+    this.maxHpElement = document.getElementById("monster-max-hp");
+    this.hurtElement = document.getElementById("monster-hp-hurt");
+
+
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHP;
+
     console.log("遇到怪獸"+this.name+"!");}
 
   attack(character){
     var damage = Math.random()*(this.ap/2)+(this.ap/2);
     super.attack(character,Math.floor(damage));
+  }
+
+  getHurt(damage){
+    super.getHurt(damage);
+    this.updateHTML(this.hpElement,this.hurtElement);
   }
 
 }
